f11ca3a893c95da0e277f450bc2e3b6af6ecee23
thalescr7 <128698680+thalescr7@users.noreply.github.com>
2024-02-15 10:46:17 -0300

    Add files via upload

---
+++ primeiroTRDEPH.py
@@ -0,0 +1,17 @@
+
+#nome = input ("Escreva o seu nome")
+#idade = input("Escreva a sua idade")
+#idade = int(idade)
+#print(f"Olá, {nome}, você tem {idade} anos.")
+#try:
+ #   idade=int(input("Escreva a sua idade"))
+#except ValueError:
+ #       print("Você nao digitou a sua idade!")
+ 
+#Numero1= input("Escreva um numero de 1 a 10")
+
+#Numero2= input("Escreva um numero de 20 a 50")
+try:
+  Numero1=int(input("Escreva um numero de 1 a 10!"))
+except ValueError:
+      print("Você nao digitou um numero de 1 a 10!")
