94f63a237eb6ab128c4f477beab7937f80da9d15
thalescr7 <128698680+thalescr7@users.noreply.github.com>
2024-02-23 11:57:09 -0300

    Add files via upload
    trabalho back and

---
+++ Inteiros.js
@@ -0,0 +1,8 @@
+let inteiro = 10;
+let flutuante = 12.5;
+let inteiro2 = 20; 
+let flutuante2 = 15.3;
+console.log(inteiro);
+console.log(flutuante);
+console.log(inteiro2);
+console.log(flutuante2);
---
+++ nome_idade.js
@@ -0,0 +1,12 @@
+//let nome = "Thales";
+//let idade = "16";
+//console.log(`O meu nome é ${nome} a idade dele é ${idade}`);
+
+
+let nome = "Thales";
+let idade = 16;
+console.log(`O meu nome é ${nome} a idade dele é ${idade}`);
+let peso = 67;
+let numero = 10;
+
+console.log(`A idade da pessoa mais o numero é igual a ${idade + numero}`)
