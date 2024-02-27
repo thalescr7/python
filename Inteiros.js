db8dce737b06e6f569181e3d06e66a200b4405cc
thalescr7 <128698680+thalescr7@users.noreply.github.com>
2024-02-27 07:48:30 -0300

    Delete Inteiros.js

--- Inteiros.js
+++
@@ -1,8 +0,0 @@
-let inteiro = 10;
-let flutuante = 12.5;
-let inteiro2 = 20; 
-let flutuante2 = 15.3;
-console.log(inteiro);
-console.log(flutuante);
-console.log(inteiro2);
-console.log(flutuante2);
