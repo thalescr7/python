59c1887df9458e92606c9060806b72ea24847779
thalescr7 <128698680+thalescr7@users.noreply.github.com>
2024-03-19 08:46:04 -0300

    Add files via upload

---
+++ Refatorado/jokenpo.py
@@ -0,0 +1,62 @@
+
+import random
+
+def jogar_jokenpo():
+    opcoes = ["pedra", "papel", "tesoura"]
+    print("Vamos jogar jokenpo?")
+    print("Escolha: pedra, papel ou tesoura")
+
+    while True:
+        escolha_jogador = input("Sua escolha:")
+        if escolha_jogador not in opcoes:
+            print("Escolha inválida. Tente novamente!")
+            continue
+
+        escolha_computador = random.choice(opcoes)
+
+        print(f"Computador escolheu: {escolha_computador}")
+
+        resultado = ganhador(escolha_jogador, escolha_computador)
+
+        resultado = ganhador(escolha_jogador, escolha_computador)
+        print(resultado)
+
+        if resultado == "Voce ganhou!":
+            vitorias += 1
+        elif resultado == "Voce perdeu!":
+            vitorias -=1 
+
+        if escolha_jogador == escolha_computador:
+            print("Empate")
+        elif(
+            (escolha_jogador == "papel" and escolha_jogador == "pedra")or
+            (escolha_jogador == "tesoura" and escolha_jogador == "papel") or
+            (escolha_jogador == "pedra" and escolha_jogador == "tesoura")  ):
+            print("Voce ganhou!")
+            
+        else:
+            print("Voce perdeu!")
+
+        jogar_novamente = input("Você deseja jogar novamente?").lower()
+        if jogar_novamente != "sim":
+            break
+
+    
+def ganhador(escolha_jogador, escolha_computador):
+
+        if escolha_jogador == escolha_computador:
+            print("Empate")
+        elif(
+            (escolha_jogador == "papel" and escolha_jogador == "pedra")or
+            (escolha_jogador == "tesoura" and escolha_jogador == "papel") or
+            (escolha_jogador == "pedra" and escolha_jogador == "tesoura")
+        ):
+            return "Voce ganhou!"
+            
+        else:
+            return "Voce perdeu!"
+    
+
+
+if __name__ == "__main__":
+    jogar_jokenpo()
